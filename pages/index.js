b59e39b0ab451b35aa081027b3baf77c7360c9ea
JakeDenley <denley420@gmail.com>
2023-03-08 12:32:46 +0800

    next.js course

---
+++ pages/index.js
@@ -0,0 +1,58 @@
+import MeetupList from "@/components/meetups/MeetupList";
+import { MongoClient } from "mongodb";
+import Head from "next/head";
+import { Fragment } from "react";
+
+export default function HomePage(props) {
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetups!"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </Fragment>
+  );
+}
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   // fetch data from an API or Database
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+
+export async function getStaticProps() {
+  // fetch data from an API or Database
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://jake_denley:ZHrOeayCTY89QvFo@cluster0.cgh1zlh.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
+}
